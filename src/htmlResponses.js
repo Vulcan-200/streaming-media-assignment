4e47dac435e5213c9e13ec0e335a310b9abfa437
Mark Starovoytov <70524900+Vulcan-200@users.noreply.github.com>
2026-01-31 03:23:52 -0500

    Completed streaming media assignment

---
+++ src/htmlResponses.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const pages =
+{
+    1: fs.readFileSync(`${__dirname}/../client/client.html`),
+    2: fs.readFileSync(`${__dirname}/../client/client2.html`),
+    3: fs.readFileSync(`${__dirname}/../client/client3.html`)
+
+}
+
+const getPage = (request, response, pageNumber) =>
+{
+    const page = pages[pageNumber];
+
+    if (!page)
+    {
+        response.writeHead(404, { 'Content-Type': 'text/plain' });
+        response.end('Page not found');
+        return;
+    }
+
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(page);
+    response.end();
+};
+
+module.exports = { getPage }
